1b5b6c070158f7682a5af3e2cbc89a77b4827ef2
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-11-28 22:56:33 -0500

    🌟ADD: Add .env and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
-
+    MongooseModule.forRoot(process.env.MONGO_DB),
     PokemonModule,
-
     CommonModule,
-
     SeedModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('AppModule loaded', process.env);
+  }
+}
