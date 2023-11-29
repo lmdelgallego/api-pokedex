9a7f258ed0f1f478a127b300417764de98f51b44
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-11-29 08:14:52 -0500

    🌟ADD: Config loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,12 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/app.config';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
---
+++ src/config/app.config.ts
@@ -0,0 +1,6 @@
+export const EnvConfiguration = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGO_DB || 'mongodb://localhost:27017/pokemon',
+  port: process.env.PORT || 3001,
+  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+});
