ccf69936c8bb28ce2a8ec261fae8daee0c526162
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-12-10 22:45:40 -0500

    🌟ADD: Add name db

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,9 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGO_DB),
+    MongooseModule.forRoot(process.env.MONGO_DB, {
+      dbName: 'pokemonDB',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
