0a3c1489ea8c27711da411a9cc54097d32a4337a
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-12-10 23:05:37 -0500

    🌟ADD: Clean code

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(process.env.PORT || 3000);
-  console.log(`App running on PORT: ${process.env.PORT || 3000}`);
+  await app.listen(process.env.PORT);
+  console.log(`App running on PORT: ${process.env.PORT}`);
 }
 bootstrap();
